cbd4ac170da1678ec4264fbb451f9a9c95cf2c8c
Savera <Level 2>
2016-03-28 15:11:12 -0700

    worked with moving, drawing, and loops.

--- src/day1/robot/FourSquare.java
+++ src/day1/robot/FourSquare.java
@@ -5,30 +5,36 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
-	// 2. Create a new Robot
-	
+
+Robot r =new Robot("batman");	
 
 	void go() {
-		// 4. Make the robot move as fast as possible
+r.penDown();
+r.setSpeed(10);
 
-		// 5. Set the pen width to 5
-
+r.setPenWidth(5);
 		// 6. Do steps #7 to #8 four times...
+for (int i = 0; i <4; i++) {
+	r.setRandomPenColor();
 
 			// 7. Set the pen color to random
 	
-			// 1. Call the drawSquare() method
+
+		drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
-
+		r.turn(90);
+}
 	}
 
-	/* 3. Fill in the code to draw a square inside the method below. */
-	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 
+	void drawSquare() {
+		for (int i = 0; i < 4; i++) {
+			r.move(100);
+		r.turn(360/4);
 		
-		
+		}
+		r.turn(1);
 	}
 
 	public static void main(String[] args) {
--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,30 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot r2d2 = new Robot("batman");
+Robot C =new Robot("mini");
 
-	
-		
+r2d2.setSpeed(5);
+r2d2.penDown();
+r2d2.setPenColor(Color.pink);
+C.setSpeed(5);
+C.setPenColor(Color.pink);
+C.penDown();
+
+C.turn(-90);
+C.move(300);
+
+for (int i = 0; i < 8; i++)
+{
+	r2d2.turn(360/8);
+	r2d2.move(100);
+}
+
+for (int i = 0; i < 3; i++)
+{
+	C.turn(360/3);
+	C.move(100);
+}
 		
 	}
 }
